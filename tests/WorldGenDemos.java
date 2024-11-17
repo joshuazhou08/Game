334a2faf262b048781494c53d0bf297756e4db9e
Joshua Zhou <joshua.zhou0808@gmail.com>
2024-11-17 12:57:58 -0800

    Generate Random Worlds For Testing

---
+++ tests/WorldGenDemos.java
@@ -0,0 +1,30 @@
+import core.World;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+public class WorldGenDemos {
+    public static void main(String[] args) {
+        int seed = 20;
+        Random random = new Random(100);
+
+        for (int i = 0; i < 10; i++) {
+            try {
+                // Pause the current thread for 5000 milliseconds (5 seconds)
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                // Catch the InterruptedException that might occur if the thread is interrupted
+                System.out.println("Interrupted: " + e.getMessage());
+            }
+            seed = random.nextInt();
+            World test = new World(seed);
+            test.createWorld();
+            test.render();
+        }
+
+        // build your own world!
+
+
+
+    }
+}
